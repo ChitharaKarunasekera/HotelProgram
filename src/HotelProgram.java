c7a046b667040b78c750e5e06edab323c086f5e1
Chithara <chithara.2019@iit.ac.lk>
2021-09-18 17:52:09 +0530

    changes in main method

--- src/HotelProgram.java
+++ src/HotelProgram.java
@@ -1,36 +1,50 @@
 
 import java.util.*;
+
 public class HotelProgram {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String roomName;
         int roomNum = 0;
-        String[] hotel = new String[7];
-    for (int x = 0; x < 6; x++ ) hotel[x] = ""; //initialise
-        initialise(hotel); //better to initialise in a procedure
-        while ( roomNum < 6 )
+
+        String[] hotel = new String[7];//Array of rooms
+
+        for (int x = 0; x < 6; x++) {
+            hotel[x] = "";//initialise
+        }
+
+        initialise(hotel); //calling initialize method
+
+        while (true)
         {
-            for (int x = 0; x < 6; x++ )
+            System.out.println("V - ‘View All Rooms ");
+
+            System.out.println("Enter room number (0-5) or 6 to stop:");
+            roomNum = input.nextInt();//get the input room number
+
+            //if the room number is not 6
+            if(roomNum != 6)
             {
-                if (hotel[x].equals("e"))System.out.println("room " + x + " is empty");
+                System.out.println("Enter name for room " + roomNum + " :");
+                roomName = input.next();//store name for room
+                hotel[roomNum] = roomName;//assign the name to array index holding the room number
             }
-            System.out.println("Enter room number (0-5) or 6 to stop:" );
-            roomNum = input.nextInt();
-            System.out.println("Enter name for room " + roomNum +" :" ) ;
-            roomName = input.next();
-            hotel[roomNum] = roomName ;
-            for (int x = 0; x < 6; x++ )
+            else
             {
-                System.out.println("room " + x + " occupied by " + hotel[x]);
+                System.out.println("system will terminate!");
+                break;
             }
         }
     }
-    
+
     //Method to initialize empty rooms
-    private static void initialise( String hotelRef[] ) {
-        for (int x = 0; x < 6; x++ ) hotelRef[x] = "e";
-        System.out.println( "initialise ");
+    private static void initialise(String hotelRef[]) {
+        for (int x = 0; x < 6; x++) {
+            hotelRef[x] = "e";
+        }
+        System.out.println("initialised ");
     }
 
-    //Method ti view all rooms
+    //Method to view all rooms
+    
 }
