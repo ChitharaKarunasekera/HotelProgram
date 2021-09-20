d68e50482de0947bb243b0614ee420dfcbfa6da7
Chithara <chithara.2019@iit.ac.lk>
2021-09-20 11:04:07 +0530

    find and delete

--- src/HotelProgram.java
+++ src/HotelProgram.java
@@ -1,11 +1,14 @@
 
+import java.awt.*;
 import java.util.*;
 
 public class HotelProgram {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        char option;
+        char option;//users option at the menu
+        int roomNum;//room number required
+        String roomName;//required room name
 
         String[] hotel = new String[7];//Array of rooms
 
@@ -34,6 +37,7 @@ public class HotelProgram {
 
             System.out.print("Select your option: ");
             option = (char) (input.next().charAt(0) & 0x5f);//Convert inout to upper case
+            System.out.println("");
 
             if (option == 'V') {
                 System.out.println("View all Rooms.");
@@ -44,13 +48,21 @@ public class HotelProgram {
             } else if (option == 'E') {
                 System.out.println("Empty rooms.");
                 //calls the isEmpty method and checks if each room is empty or not
-                for (int i=0; i<hotel.length-1; i++)
-                {
-                    if(isEmpty(hotel,i) != -1)
-                    {
+                for (int i = 0; i < hotel.length - 1; i++) {
+                    if (isEmpty(hotel, i) != -1) {
                         System.out.println("Room " + i);//Display room number is the function returns the number
                     }
                 }
+            } else if (option == 'D') {
+                System.out.println("Delete a customer.");
+                System.out.print("Enter Room number: ");
+                roomNum = input.nextInt();//store the room number the user want to delete
+                delete(hotel, roomNum);
+            } else if (option == 'F') {
+                System.out.println("Find a customer.");
+                System.out.print("Enter the name for room: ");
+                roomName = input.next();//get the name to find room number
+                find(hotel, roomName);
             } else if (option == 'S') {
                 System.out.println("System will terminate!");
                 break;
@@ -78,7 +90,7 @@ public class HotelProgram {
     //Method to view all rooms
     private static void view(String hotelRef[]) {
         for (int x = 0; x < 6; x++) {
-            if (isEmpty(hotelRef,x) != -1) {
+            if (isEmpty(hotelRef, x) != -1) {
                 //if the method's return value is not -1 the room is empty
                 System.out.println("room " + x + " is empty");
             } else {
@@ -90,17 +102,17 @@ public class HotelProgram {
 
     //Method to add customers
     private static void add(String hotelRef[]) {
-        Scanner input = new Scanner(System.in); //
+        Scanner input = new Scanner(System.in);
         String roomName;
         int roomNum;
 
         while (true) {
-            System.out.print("Enter room number (0-5) or 6 to stop:");
+            System.out.print("Enter room number (0-5) or 6 to stop: ");
             roomNum = input.nextInt();//get the input room number
 
             //if the room number is between the existing room numbers
             if (roomNum < 6 && roomNum >= 0) {
-                System.out.print("Enter name for room " + roomNum + " :");
+                System.out.print("Enter name for room " + roomNum + " : ");
                 roomName = input.next();//store name for room
                 hotelRef[roomNum] = roomName;//assign the name to array index holding the room number
             }
@@ -112,6 +124,45 @@ public class HotelProgram {
             //Entered an invalid room number
             else {
                 System.out.println("\nInvalid room number!\n");
+            }
+        }
+    }
+
+    private static void delete(String hotelRef[], int roomNum) {
+        Scanner input = new Scanner(System.in);
+        char ans;
+
+        while (true) {
+            //check if the room is not empty
+            if (isEmpty(hotelRef, roomNum) == -1) {
+                //confirm the request
+                System.out.print("Are you sure you want to remove Customer " + hotelRef[roomNum] + " from room" + roomNum + "(y/n)?");
+                ans = (char) (input.next().charAt(0) & 0x5f);//Convert inout to upper case
+
+                //remove the customer if the request was confirmed or else cancel the requests
+                if (ans == 'Y') {
+                    System.out.println(hotelRef[roomNum] + " was removed from room " + roomNum);
+                    hotelRef[roomNum] = "e";
+                    break;
+                } else {
+                    System.out.println("Request has been canceled! Customer " + hotelRef[roomNum] + " was not removed.");
+                    break;
+                }
+            } else {
+                System.out.println("\nSorry! This room is already empty.\n");
+            }
+        }
+    }
+
+    //Function to find customer from name
+    private static void find(String hotelRef[], String roomName) {
+        for (int i = 0; i < hotelRef.length-1; i++) {
+            if (hotelRef[i].equalsIgnoreCase(roomName)){
+                System.out.println("Customer " + hotelRef[i] + " is in room " + i);
+                break;
+            }
+            if (i == hotelRef.length-2){
+                System.out.println("Sorry! Customer " + roomName + " does not exist.");
             }
         }
     }
