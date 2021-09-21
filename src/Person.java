2d87208ebc198a91dc2540ffe6ad28ac31441d69
Chithara <chithara.2019@iit.ac.lk>
2021-09-21 19:19:06 +0530

    creating classes

---
+++ src/Person.java
@@ -0,0 +1,39 @@
+public class Person {
+    //Information of the paying guest
+    private String firstName;
+    private String lastName;
+    private String creditCardNo;
+
+    public Person(String firstName, String lastName, String creditCardNo){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.creditCardNo = creditCardNo;
+    }
+
+    //Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCreditCardNo() {
+        return creditCardNo;
+    }
+
+
+    //Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCreditCardNo(String creditCardNo) {
+        this.creditCardNo = creditCardNo;
+    }
+}
--- src/Room.java
+++ src/Room.java
@@ -1,21 +1,51 @@
 public class Room {
     private final int roomNum;
     private String roomName;
+    private int noOfGuests;
+    private Person payingGuest;
 
     public Room(int roomNum, String roomName){
         this.roomNum = roomNum;
         this.roomName = roomName;
+        this.noOfGuests = 0;
+        this.payingGuest = null;
     }
 
+    //Getters
     public int getRoomNum() {
         return roomNum;
+    }
+
+    public int getNoOfGuests() {
+        return noOfGuests;
+    }
+
+    public Person getPayingGuest() {
+        return payingGuest;
     }
 
     public String getRoomName(){
         return roomName;
     }
 
+    //Setters
     public void setRoomName(String roomName){
         this.roomName = roomName;
+    }
+
+    public void setNoOfGuests(int noOfGuests) {
+        this.noOfGuests = noOfGuests;
+    }
+
+    public void setPayingGuest(Person payingGuest) {
+        this.payingGuest = payingGuest;
+    }
+
+    public void addPayingGuest(String firstName, String lastName, String creditCardNo){
+        this.payingGuest = new Person(firstName, lastName, creditCardNo);
+    }
+
+    public void deletePayingGuest(){
+        this.payingGuest = null;
     }
 }
